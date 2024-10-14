9749036ad0688e01ef05fe56455cb6ade8a300d2
Jonathan-Kes <jk081506@wgcloud.org>
2024-10-14 11:17:45 -0500

    code

--- pythagorean.py
+++ pythagorean.py
@@ -2,3 +2,28 @@
 # Alpha version 1.0
 # Coder: Jonathan Kessler
 # Date: 2024-10-14
+import math as Math
+# declares variables 
+sideA = 0
+sideB = 0
+sideC = 0
+
+# equatoin: a*a + b*b = c*c
+
+# declare functions
+def solver(a,b):
+    c = (Math.pow(a,2)) + (Math.pow(b,2))
+    sideC = Math.sqrt(c)
+    return sideC
+
+
+
+# get user input
+sideA=float(input("Enter the length of side A: "))
+sideB=float(input("Enter the length of side B: "))
+
+# solve equation
+print(solver(sideA,sideB))
+
+
+# output answer
